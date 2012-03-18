f4041b1a19ae5ba7f7e2a12a017df16d38b58a1d
Ben Burkert <ben@benburkert.com>
2012-03-18 15:45:26 -0700

    Refactor the certificate generators.

--- test/generate_cert.go
+++ test/generate_cert.go
@@ -18,38 +18,40 @@ type Error struct {
 	message string
 }
 
+type templateDecorator func(*x509.Certificate) (*x509.Certificate, *x509.Certificate, error)
+
 func GenerateCA(hostname string) (*x509.Certificate, error) {
-	return buildCert(func(template *x509.Certificate, key *rsa.PrivateKey) ([]byte, error) {
+	return buildCert(func(template *x509.Certificate) (*x509.Certificate, *x509.Certificate, error) {
 		template.Subject.CommonName = hostname
 		template.AuthorityKeyId = template.SubjectKeyId
 		template.KeyUsage = x509.KeyUsageCertSign
 		template.BasicConstraintsValid = true
 		template.IsCA = true
 
-		return x509.CreateCertificate(rand.Reader, template, template, &key.PublicKey, key)
+		return template, template, nil
 	})
 }
 
 func GenerateIntermediate(hostname string, caCert *x509.Certificate) (*x509.Certificate, error) {
-	return buildCert(func(template *x509.Certificate, key *rsa.PrivateKey) ([]byte, error) {
+	return buildCert(func(template *x509.Certificate) (*x509.Certificate, *x509.Certificate, error) {
 		template.Subject.CommonName = hostname
 		template.AuthorityKeyId = caCert.SubjectKeyId
 		template.KeyUsage = x509.KeyUsageCertSign
 
-		return x509.CreateCertificate(rand.Reader, template, caCert, &key.PublicKey, key)
+		return template, caCert, nil
 	})
 }
 
 func GenerateCert(hostname string, parent *x509.Certificate) (*x509.Certificate, error) {
-	return buildCert(func(template *x509.Certificate, key *rsa.PrivateKey) ([]byte, error) {
+	return buildCert(func(template *x509.Certificate) (*x509.Certificate, *x509.Certificate, error) {
 		template.Subject.CommonName = hostname
 		template.AuthorityKeyId = parent.SubjectKeyId
 
-		return x509.CreateCertificate(rand.Reader, template, parent, &key.PublicKey, key)
+		return template, parent, nil
 	})
 }
 
-func buildCert(finisher func(*x509.Certificate, *rsa.PrivateKey) ([]byte, error)) (*x509.Certificate, error) {
+func buildCert(decorator templateDecorator) (*x509.Certificate, error) {
 	priv, err := rsa.GenerateKey(rand.Reader, 1024)
 	if err != nil {
 		return nil, err
@@ -74,8 +76,12 @@ func buildCert(finisher func(*x509.Certificate, *rsa.PrivateKey) ([]byte, error)
 		SubjectKeyId: keyId,
 	}
 
-	derBytes, err := finisher(&template, priv)
+	cert, parent, err := decorator(&template)
+	if err != nil {
+		return nil, err
+	}
 
+	derBytes, err := x509.CreateCertificate(rand.Reader, cert, parent, &priv.PublicKey, priv)
 	if err != nil {
 		return nil, err
 	}
